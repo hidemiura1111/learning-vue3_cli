bfc5ec49cc3c7594baa5f52d9b4ae3390d76c659
hidems <miura.eleven@gmail.com>
2023-09-06 23:49:20 +0200

    returned value in setup()

--- src/views/CompositionTestView.vue
+++ src/views/CompositionTestView.vue
@@ -1,16 +1,29 @@
 <template>
   <div>
     <h1>Composition Test</h1>
+    <p>{{ name }}</p>
+    <p>{{ age }}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: "CompositionTestView",
-  data() { },
   setup() {
     console.log("setup")
     console.log(this)
+    let name = "Name from Seup"
+    const age = 20
+    return {
+      name,
+      age
+    }
+  },
+  data() {
+    return {
+      nameData: "Name from Data",
+      ageData: 20
+    }
   },
   created() {
     console.log("created")
