700693b2364803182dd2ad22527899832cee9044
hidems <miura.eleven@gmail.com>
2023-10-12 00:25:22 +0200

    Composition Function

--- src/views/CompositionFunctionTestView.vue
+++ src/views/CompositionFunctionTestView.vue
@@ -1,11 +1,54 @@
 <template>
   <div>
     <div>Composition Function Test</div>
+    <div>
+      <p>Name: {{ item.name }}</p>
+      <p>Price: {{ item.price }}</p>
+      <p>Amount: {{ item.amount }}</p>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+      <p>Total Price: {{ totalPrice }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import { reactive, computed } from 'vue'
+
+const useCounter = item => {
+  const increment = () => {
+    item.amount++
+  }
+  const decrement = () => {
+    item.amount--
+  }
+  const totalPrice = computed(() => {
+    return item.price * item.amount
+  })
+
+  return {
+    increment,
+    decrement,
+    totalPrice
+  }
+}
 export default {
+  setup() {
+    const item = reactive({
+      name: 'Product',
+      price: 100,
+      amount: 0
+    })
+
+    const { increment, decrement, totalPrice } = useCounter(item)
+
+    return {
+      item,
+      increment,
+      decrement,
+      totalPrice
+    }
+  }
 
 }
 </script>
