ee059653a035d6596852e1bb602a3803175c4373
hidems <miura.eleven@gmail.com>
2023-10-16 22:29:48 +0200

    Reouter Test

--- src/views/RouterTestView.vue
+++ src/views/RouterTestView.vue
@@ -2,10 +2,31 @@
   <div>
     Router Test
   </div>
+  <button @click="goHome">Go Home</button>
+  <button @click="routeCheckPath">Route Check Path</button>
 </template>
 
 <script>
+import { useRoute, useRouter } from 'vue-router';
+
 export default {
+  setup() {
+    const route = useRoute();
+    const router = useRouter();
+
+    const goHome = () => {
+      router.push('/')
+    }
+
+    const routeCheckPath = () => {
+      console.log(route.path)
+    }
+
+    return {
+      goHome,
+      routeCheckPath
+    }
+  }
 
 }
 </script>
