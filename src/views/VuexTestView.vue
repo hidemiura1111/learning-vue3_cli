8267fb569f326692faaba77dd0ffbd5df9a9461a
hidems <miura.eleven@gmail.com>
2023-10-19 21:44:29 +0200

    Vuex and Vue3

--- src/views/VuexTestView.vue
+++ src/views/VuexTestView.vue
@@ -1,10 +1,30 @@
 <template>
   <div>Vuex Test</div>
+  <div>{{ count }}</div>
+  <button @click="increment">+</button>
 </template>
 
 <script>
-export default {
+import { computed } from 'vue'
+import { useStore } from 'vuex'
 
+export default {
+  setup() {
+    const store = useStore()
+
+    const count = computed(() => {
+      return store.state.count
+    })
+
+    const increment = () => {
+      store.commit('increment')
+    }
+
+    return {
+      count,
+      increment
+    }
+  }
 }
 </script>
 
