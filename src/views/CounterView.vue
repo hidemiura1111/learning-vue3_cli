8e1400906666b8ec1721cdb88f3290a72977a496
hidems <miura.eleven@gmail.com>
2023-11-18 23:00:27 +0100

    Counter, View

--- src/views/CounterView.vue
+++ src/views/CounterView.vue
@@ -1,13 +1,34 @@
 <template>
-  <div>Counter</div>
+  <div id="app">
+    <p>
+      Clicked: {{ count }} times
+    </p>
+    <p>
+      count is {{ evenOrOdd }}.
+    </p>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementIfOdd">Increment if odd</button>
+    <button @click="incrementAsync">Increment async</button>
+  </div>
 </template>
 
 <script>
-export default {
+import { computed } from 'vue'
+import { useStore } from 'vuex'
 
+export default {
+  setup () {
+    const store = useStore()
+
+    return {
+      count: computed(() => store.state.count),
+      evenOrOdd: computed(() => store.getters.evenOrOdd),
+      increment: () => store.dispatch('increment'),
+      decrement: () => store.dispatch('decrement'),
+      incrementIfOdd: () => store.dispatch('incrementIfOdd'),
+      incrementAsync: () => store.dispatch('incrementAsync')
+    }
+  }
 }
 </script>
-
-<style>
-
-</style>
